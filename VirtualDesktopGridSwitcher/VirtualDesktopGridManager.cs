ff118acb13fdc46c74e41194b7b6046023061b74
squidly <squidly@localhost>
2016-05-09 05:37:02 +1000

    Fix move window and make foreground change handler thread-safe

--- VirtualDesktopGridSwitcher/VirtualDesktopGridManager.cs
+++ VirtualDesktopGridSwitcher/VirtualDesktopGridManager.cs
@@ -11,6 +11,7 @@ using WindowsDesktop;
 using System.IO;
 using System.Diagnostics;
 using System.Drawing;
+using System.Threading;
 
 namespace VirtualDesktopGridSwitcher {
 
@@ -429,22 +430,24 @@ namespace VirtualDesktopGridSwitcher {
             ShowWindow(hwnd, winInfo.ShowCmd);
         }
 
+        Mutex mutex = new Mutex();
         void ForegroundWindowChanged(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime) {
-            if (desktops != null)
-            {
-                var desktopId = Current;
-                var desktop = VirtualDesktop.FromHwnd(hwnd);
-                if (desktop != null) {
-                    desktopId = desktopIdLookup[desktop];
-                }
-                Debug.WriteLine("Foreground " + Current + " " + (desktop != null ? desktopIdLookup[desktop].ToString() : "?") + " " + hwnd);
+            lock (mutex) {
+                if (desktops != null) {
+                    var desktopId = Current;
+                    var desktop = VirtualDesktop.FromHwnd(hwnd);
+                    if (desktop != null) {
+                        desktopId = desktopIdLookup[desktop];
+                    }
+                    Debug.WriteLine("Foreground " + Current + " " + (desktop != null ? desktopIdLookup[desktop].ToString() : "?") + " " + hwnd);
 
-                if (IsWindowDefaultBrowser(hwnd, settings.GetBrowserToActivateInfo())) {
-                    Debug.WriteLine("Browser " + Current + " " + desktopIdLookup[VirtualDesktop.Current] + " " + desktopId + " " + hwnd);
-                    lastActiveBrowserWindows[desktopId] = hwnd;
+                    if (IsWindowDefaultBrowser(hwnd, settings.GetBrowserToActivateInfo())) {
+                        Debug.WriteLine("Browser " + Current + " " + desktopIdLookup[VirtualDesktop.Current] + " " + desktopId + " " + hwnd);
+                        lastActiveBrowserWindows[desktopId] = hwnd;
+                    }
                 }
+                //ReleaseModifierKeys();
             }
-            //ReleaseModifierKeys();
         }
 
         private static string GetWindowExeName(IntPtr hwnd) {
@@ -552,7 +555,7 @@ namespace VirtualDesktopGridSwitcher {
                     }
                 }
                 Debug.WriteLine("Move " + hwnd + " from " + Current + " to " + index);
-                if (VirtualDesktopHelper.MoveToDesktop(hwnd, desktops[index])) {
+                if (!VirtualDesktopHelper.MoveToDesktop(hwnd, desktops[index])) {
                     this.VDMHelper.MoveWindowToDesktop(hwnd, desktops[index].Id);
                 }
             }
